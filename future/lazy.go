6682bbb97a99ec1eda04886eeebad9f5c1450ef2
kortschak <kortschak@zoidberg.mbs.adelaide.edu.au>
2011-12-03 14:38:25 +1030

    Reaper for lazy function goroutine.

--- future/lazy.go
+++ future/lazy.go
@@ -15,8 +15,6 @@ package future
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import "runtime"
-
 // Evaluation function
 type Eval func(...interface{}) (interface{}, []interface{})
 
@@ -25,34 +23,29 @@ type Eval func(...interface{}) (interface{}, []interface{})
 // It is probably worth while wrapping this in a function that returns the result as the type you are actually using.
 // e.g.
 //
-//   func LazilyString(f Eval, rc chan string, init ...string) func() (string) {
+//   func LazilyString(f Eval, rc chan string, reaper <-chan struct{}, init ...string) func() (string) {
 //       return func() string {
-//           return Lazily(f, rc, init...)().(string)
+//           return Lazily(f, rc, reaper, init...)().(string)
 //       }
 //   }
 //
-// Lazy functions will result in goroutine leaks. TODO
+// Lazy functions are terminated by closing the reaper channel. nil should be passed as
+// a reaper for perpetual lazy functions.
 //
 // Function to generate the lazy evaluator
-func Lazily(f Eval, rc chan interface{}, init ...interface{}) func() interface{} {
+func Lazily(f Eval, rc chan interface{}, reaper <-chan struct{}, init ...interface{}) func() interface{} {
 	go func() {
-		defer func() {
-			if r := recover(); r != nil {
-				if e, ok := r.(runtime.Error); ok {
-					if e.Error() == "runtime error: send on closed channel" {
-						return
-					}
-				}
-				panic(r)
-			}
-		}()
-
+		defer close(rc)
 		var state []interface{} = init
 		var result interface{}
 
 		for {
 			result, state = f(state...)
-			rc <- result
+			select {
+			case rc <- result:
+			case <-reaper:
+				return
+			}
 		}
 	}()
 
