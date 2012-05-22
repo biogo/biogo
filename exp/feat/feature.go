5970fccf418648879956e2d463e39787fbf3c646
kortschak <dan.kortschak@adelaide.edu.au>
2012-05-22 15:17:22 +0930

    Define base feature interfaces

---
+++ exp/feat/feature.go
@@ -0,0 +1,102 @@
+// Copyright ©2012 Dan Kortschak <dan.kortschak@adelaide.edu.au>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Package feat provides the base for storage and manipulation of biological interval information.
+package feat
+
+type Conformationer interface {
+	Conformation() Conformation
+}
+
+type Conformation int8
+
+func (c Conformation) String() string {
+	switch c {
+	case UndefinedConformation:
+		return "undefined"
+	case Linear:
+		return "linear"
+	case Circular:
+		return "circular"
+	}
+	panic("feat: illegal conformation")
+}
+
+const (
+	UndefinedConformation Conformation = iota - 1
+	Linear
+	Circular
+)
+
+type Orienter interface {
+	Orientation() Orientation
+}
+
+type Orientation int8
+
+func (o Orientation) String() string {
+	switch o {
+	case Reverse:
+		return "reverse"
+	case NotOriented:
+		return "not oriented"
+	case Forward:
+		return "forward"
+	}
+	panic("feat: illegal orientation")
+}
+
+const (
+	Reverse Orientation = iota - 1
+	NotOriented
+	Forward
+)
+
+type Feature interface {
+	Start() int
+	End() int
+	Len() int
+	Name() string
+	Description() string
+	Location() Feature
+}
+
+type Offsetter interface {
+	Offset(int)
+}
+
+type Mutable interface {
+	SetStart(int)
+	SetEnd(int)
+}
+
+type LocationSetter interface {
+	SetLocation(Feature)
+}
+
+type Pair interface {
+	Features() [2]Feature
+	Invert() // Swap base feature and partner feature.
+}
+
+type Set interface {
+	Add(...Feature)
+	Features() []Feature
+}
+
+type Collection interface {
+	Set
+	Location() Feature
+}
