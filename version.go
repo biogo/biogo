bd0f09a792924477d95ddce2edff3059e6f32acc
kortschak <dan.kortschak@adelaide.edu.au>
2013-02-06 13:11:19 +1030

    Bump version to beta

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
-// Copyright ©2011-2012 The bíogo Authors. All rights reserved.
+// Copyright ©2011-2013 The bíogo Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package biogo
 
-const Version = "alpha"
+const Version = "beta"
