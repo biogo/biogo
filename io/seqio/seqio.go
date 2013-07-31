37f172345f4c0b69271f64bab84c47df6f48b400
kortschak <dan.kortschak@adelaide.edu.au>
2013-07-31 12:13:53 +0930

    Add sequence Scanner type
    This is based on the bufio Scanner, but is simplified greatly by the
    fact that we hand the tokenisation task to the provided seqio.Reader.

--- io/seqio/seqio.go
+++ io/seqio/seqio.go
@@ -5,7 +5,11 @@
 // Package seqio provides interfaces for sequence I/O functions.
 package seqio
 
-import "code.google.com/p/biogo/seq"
+import (
+	"code.google.com/p/biogo/seq"
+
+	"io"
+)
 
 // A SequenceAppender is a generic sequence type that can append elements.
 type SequenceAppender interface {
@@ -17,7 +21,8 @@ type SequenceAppender interface {
 
 // Reader is the common seq.Sequence reader interface.
 type Reader interface {
-	// Read reads a seq.Sequence, returning any error that occurs during the read.
+	// Read reads a seq.Sequence, returning the sequence and any error that
+	// occurred during the read.
 	Read() (seq.Sequence, error)
 }
 
@@ -27,3 +32,41 @@ type Writer interface {
 	// error that occurs during the write.
 	Write(seq.Sequence) (int, error)
 }
+
+// Scanner wraps a Reader to provide a convenient loop interface for reading sequence data.
+// Successive calls to the Scan method will step through the sequences of the provided
+// Reader. Scanning stops unrecoverably at EOF or the first error.
+//
+// Note that it is possible for a Reader to return a valid sequence and a non-nil error. So
+// programs that need more control over error handling should use a Reader directly instead.
+type Scanner struct {
+	r   Reader
+	seq seq.Sequence
+	err error
+}
+
+// NewScanner returns a Scanner to read from r.
+func NewScanner(r Reader) *Scanner { return &Scanner{r: r} }
+
+// Scan advances the Scanner past the next sequence, which will then be available through
+// the Seq method. It returns false when the scan stops, either by reaching the end of the
+// input or an error. After Scan returns false, the Err method will return any error that
+// occurred during scanning, except that if it was io.EOF, Err will return nil.
+func (s *Scanner) Scan() bool {
+	if s.err != nil {
+		return false
+	}
+	s.seq, s.err = s.r.Read()
+	return s.err == nil
+}
+
+// Err returns the first non-EOF error that was encountered by the Scanner.
+func (s *Scanner) Err() error {
+	if s.err == io.EOF {
+		return nil
+	}
+	return s.err
+}
+
+// Seq returns the most recent sequence read by a call to Scan.
+func (s *Scanner) Seq() seq.Sequence { return s.seq }
