f2f9e4695fba599b905df60802ab50b0bc44f4b5
kortschak <dan.kortschak@adelaide.edu.au>
2013-08-05 15:21:54 +0930

    featio.Scanner
    This is the same model as for seqio.Scanner.

--- io/featio/featio.go
+++ io/featio/featio.go
@@ -2,10 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package featio provides interfaces for feature I/O functions. 
+// Package featio provides interfaces for feature I/O functions.
 package featio
 
-import "code.google.com/p/biogo/feat"
+import (
+	"code.google.com/p/biogo/feat"
+
+	"io"
+)
 
 // Reader is the common feat.Feature reader interface.
 type Reader interface {
@@ -19,3 +23,41 @@ type Writer interface {
 	// error that occurs during the write.
 	Write(feat.Feature) (n int, err error)
 }
+
+// Scanner wraps a Reader to provide a convenient loop interface for reading feature data.
+// Successive calls to the Scan method will step through the features of the provided
+// Reader. Scanning stops unrecoverably at EOF or the first error.
+//
+// Note that it is possible for a Reader to return a valid feature and a non-nil error. So
+// programs that need more control over error handling should use a Reader directly instead.
+type Scanner struct {
+	r   Reader
+	f   feat.Feature
+	err error
+}
+
+// NewScanner returns a Scanner to read from r.
+func NewScanner(r Reader) *Scanner { return &Scanner{r: r} }
+
+// Scan advances the Scanner past the next feature, which will then be available through
+// the Feat method. It returns false when the scan stops, either by reaching the end of the
+// input or an error. After Scan returns false, the Err method will return any error that
+// occurred during scanning, except that if it was io.EOF, Err will return nil.
+func (s *Scanner) Scan() bool {
+	if s.err != nil {
+		return false
+	}
+	s.f, s.err = s.r.Read()
+	return s.err == nil
+}
+
+// Err returns the first non-EOF error that was encountered by the Scanner.
+func (s *Scanner) Err() error {
+	if s.err == io.EOF {
+		return nil
+	}
+	return s.err
+}
+
+// Feat returns the most recent feature read by a call to Scan.
+func (s *Scanner) Feat() feat.Feature { return s.f }
---
+++ io/featio/featio_test.go
@@ -0,0 +1,67 @@
+// Copyright ©2011-2013 The bíogo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package featio_test
+
+import (
+	"code.google.com/p/biogo/io/featio"
+	"code.google.com/p/biogo/io/featio/gff"
+	"code.google.com/p/biogo/seq"
+
+	"bytes"
+	check "launchpad.net/gocheck"
+	"testing"
+)
+
+// Helpers
+func floatPtr(f float64) *float64 { return &f }
+
+// Tests
+func Test(t *testing.T) { check.TestingT(t) }
+
+type S struct{}
+
+var _ = check.Suite(&S{})
+
+func (s *S) TestReadGFF(c *check.C) {
+	for i, g := range []struct {
+		gff  string
+		feat []*gff.Feature
+	}{
+		{
+			gff: `SEQ1	EMBL	atg	103	105	.	+	0
+SEQ1	EMBL	exon	103	172	.	+	0
+SEQ1	EMBL	splice5	172	173	.	+	.
+SEQ1	netgene	splice5	172	173	0.94	+	.
+SEQ1	genie	sp5-20	163	182	2.3	+	.
+SEQ1	genie	sp5-10	168	177	2.1	+	.
+SEQ2	grail	ATG	17	19	2.1	-	0
+`,
+			feat: []*gff.Feature{
+				{SeqName: "SEQ1", Source: "EMBL", Feature: "atg", FeatStart: 102, FeatEnd: 105, FeatScore: nil, FeatFrame: gff.Frame0, FeatStrand: seq.Plus},
+				{SeqName: "SEQ1", Source: "EMBL", Feature: "exon", FeatStart: 102, FeatEnd: 172, FeatScore: nil, FeatFrame: gff.Frame0, FeatStrand: seq.Plus},
+				{SeqName: "SEQ1", Source: "EMBL", Feature: "splice5", FeatStart: 171, FeatEnd: 173, FeatScore: nil, FeatFrame: gff.NoFrame, FeatStrand: seq.Plus},
+				{SeqName: "SEQ1", Source: "netgene", Feature: "splice5", FeatStart: 171, FeatEnd: 173, FeatScore: floatPtr(0.94), FeatFrame: gff.NoFrame, FeatStrand: seq.Plus},
+				{SeqName: "SEQ1", Source: "genie", Feature: "sp5-20", FeatStart: 162, FeatEnd: 182, FeatScore: floatPtr(2.3), FeatFrame: gff.NoFrame, FeatStrand: seq.Plus},
+				{SeqName: "SEQ1", Source: "genie", Feature: "sp5-10", FeatStart: 167, FeatEnd: 177, FeatScore: floatPtr(2.1), FeatFrame: gff.NoFrame, FeatStrand: seq.Plus},
+				{SeqName: "SEQ2", Source: "grail", Feature: "ATG", FeatStart: 16, FeatEnd: 19, FeatScore: floatPtr(2.1), FeatFrame: gff.Frame0, FeatStrand: seq.Minus},
+			},
+		},
+	} {
+		sc := featio.NewScanner(
+			gff.NewReader(
+				bytes.NewBufferString(g.gff),
+			),
+		)
+
+		var j int
+		for sc.Scan() {
+			f := sc.Feat()
+			c.Check(f, check.DeepEquals, g.feat[j], check.Commentf("Test: %d Line: %d", i, j+1))
+			j++
+		}
+		c.Check(sc.Err(), check.Equals, nil)
+		c.Check(j, check.Equals, len(g.feat))
+	}
+}
