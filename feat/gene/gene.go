843fba5c7c9ca0c0c621bd0734fff3ba73d0580b
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2014-11-07 16:10:26 -0500

    Create gene package

---
+++ feat/gene/gene.go
@@ -0,0 +1,459 @@
+// Copyright ©2015 The bíogo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package gene contains the types and methods to handle the definition of a
+// gene. A gene is a union of genomic sequences encoding a coherent set of
+// potentially overlapping functional products. Since the package is located
+// under the feat namespace, we define gene to correspond to a specific
+// genomic region (has genomic coordinates).
+//
+// The package also contain types to describe gene transcripts. Transcripts
+// can be coding and non-coding. Coding transcripts have functional regions
+// (5'UTR, CDS and 3'UTR) and consist of exons.
+package gene
+
+import (
+	"code.google.com/p/biogo/feat"
+
+	"errors"
+	"sort"
+)
+
+const maxInt = int(^uint(0) >> 1) // The maximum int value.
+
+// Interface defines the gene interface.
+type Interface interface {
+	feat.Feature
+	feat.Orienter
+	feat.Set
+	SetFeatures(...feat.Feature) error
+}
+
+// Transcript is the interface for a gene transcript.
+type Transcript interface {
+	feat.Feature
+	feat.Orienter
+	Exons() Exons
+	Introns() Introns
+	SetExons(...Exon) error
+}
+
+// TranscriptsOf scans a feat.Set and returns any Transcripts that it finds.
+func TranscriptsOf(s feat.Set) []Transcript {
+	var ts []Transcript
+	for _, f := range s.Features() {
+		if t, ok := f.(Transcript); ok {
+			ts = append(ts, t)
+		}
+	}
+	return ts
+}
+
+// A Gene occupies a specific region on the genome and may have 0 or more
+// features, including transcripts, associated with it. The gene is tightly
+// coupled with its fetures in the sense that the gene boundaries are defined
+// by the features. By definition one of the fetures must always start at
+// position 0 relative to the gene and this or another one has to end at the
+// end of the gene. The former is asserted when features are set and the
+// latter is guarranted by setting the gene end at the largest end of the
+// features.
+type Gene struct {
+	ID     string
+	Chrom  feat.Feature
+	Offset int
+	Orient feat.Orientation
+	Desc   string
+	length int
+	feats  []feat.Feature
+}
+
+// Start returns the gene start on the chromosome.
+func (g *Gene) Start() int { return g.Offset }
+
+// End returns the gene end on the chromosome.
+func (g *Gene) End() int { return g.Offset + g.Len() }
+
+// Len returns the length of the gene.
+func (g *Gene) Len() int { return g.length }
+
+// Name returns the gene name. Currently the same as the id.
+func (g *Gene) Name() string { return g.ID }
+
+// Description returns a description for the gene.
+func (g *Gene) Description() string { return g.Desc }
+
+// Location returns the location of the gene. Namely the chromosome.
+func (g *Gene) Location() feat.Feature { return g.Chrom }
+
+// Orientation returns the orientation of the gene relative to the chromosome.
+func (g *Gene) Orientation() feat.Orientation { return g.Orient }
+
+// Features returns all features added to the gene.
+func (g *Gene) Features() []feat.Feature { return g.feats }
+
+// SetFeatures sets the gene features. Internally, it verifies that their
+// Location is the gene and that one of them has zero Start. If an error
+// occurs it is returned and the features are not set.
+func (g *Gene) SetFeatures(feats ...feat.Feature) error {
+	pos := maxInt
+	end := 0
+	for _, f := range feats {
+		if f.Location() != g {
+			return errors.New("transcript location does not match the gene")
+		}
+		if f.Start() < pos {
+			pos = f.Start()
+		}
+		if f.End() > end {
+			end = f.End()
+		}
+	}
+	if pos != 0 {
+		return errors.New("no transcript with 0 start on gene")
+	}
+	g.length = end - pos
+	g.feats = feats
+	return nil
+}
+
+// A NonCodingTranscript is a gene transcript that has no coding potential. It
+// can be located on any feat.Feature such as a gene or a chromosome. The
+// concept of exons is tighly coupled with the NonCodingTranscript in the
+// sense that the transcript borders are basically defined by the contained
+// exons. By definition one of the exons must always start at position 0
+// relative to the transcript and this or another one must end at the end of
+// transcript. The former is asserted when exons are set and the latter is
+// guarranted by setting the transcript end at the end of the last exon.
+type NonCodingTranscript struct {
+	ID     string
+	Loc    feat.Feature
+	Offset int
+	Orient feat.Orientation
+	Desc   string
+	exons  Exons
+}
+
+// Start returns the transcript start relative to Location.
+func (t *NonCodingTranscript) Start() int { return t.Offset }
+
+// End returns the transcript end relative to Location.
+func (t *NonCodingTranscript) End() int { return t.Offset + t.exons.End() }
+
+// Len returns the length of the transcript.
+func (t *NonCodingTranscript) Len() int { return t.End() - t.Start() }
+
+// Name returns the transcript name. Currently the same as the id.
+func (t *NonCodingTranscript) Name() string { return t.ID }
+
+// Description returns a description for the transcript.
+func (t *NonCodingTranscript) Description() string { return t.Desc }
+
+// Location returns the location of the transcript. Can be any feat.Feature
+// such as a gene or a chromosome.
+func (t *NonCodingTranscript) Location() feat.Feature { return t.Loc }
+
+// Orientation returns the orientation of the transcript relative to Location.
+func (t *NonCodingTranscript) Orientation() feat.Orientation { return t.Orient }
+
+// Exons returns a typed slice with the transcript exons.
+func (t *NonCodingTranscript) Exons() Exons { return t.exons }
+
+// Introns returns a typed slice with the transcript introns.
+func (t *NonCodingTranscript) Introns() Introns { return t.exons.Introns() }
+
+// SetExons sets the transcript exons. Internally, it sorts exons by Start,
+// verifies that their Location is the transcript, that they are not
+// overlapping and that one has zero Start. If an error occurs it is returned
+// and the exons are not set.
+func (t *NonCodingTranscript) SetExons(exons ...Exon) error {
+	exons, err := buildExonsFor(t, exons...)
+	if err != nil {
+		return err
+	}
+	t.exons = exons
+	return nil
+}
+
+// A CodingTranscript is a gene transcript that has coding potential. It can
+// be located on any feat.Feature such as a gene or a chromosome. The concept
+// of exons is tighly coupled with the CodingTranscript in the sense that
+// the transcript borders are basically defined by the contained exons. By
+// definition one of the exons must always start at position 0 relative to the
+// transcript and this or another one must end at the transcript end. The
+// former is asserted when exons are set and the latter is guarranted by
+// setting the transcript end at the end of the last exon.
+type CodingTranscript struct {
+	ID       string
+	Loc      feat.Feature
+	Offset   int
+	Orient   feat.Orientation
+	Desc     string
+	CDSstart int
+	CDSend   int
+	exons    Exons
+}
+
+// Start returns the transcript start relative to Location.
+func (t *CodingTranscript) Start() int { return t.Offset }
+
+// End returns the transcript end relative to Location.
+func (t *CodingTranscript) End() int { return t.Offset + t.exons.End() }
+
+// Len returns the length of the transcript.
+func (t *CodingTranscript) Len() int { return t.End() - t.Start() }
+
+// Name returns the transcript name. Currently the same as the id.
+func (t *CodingTranscript) Name() string { return t.ID }
+
+// Description returns a description for the transcript.
+func (t *CodingTranscript) Description() string { return t.Desc }
+
+// Location returns the location of the transcript. Can be any feat.Feature
+// such as a gene or a chromosome.
+func (t *CodingTranscript) Location() feat.Feature { return t.Loc }
+
+// Orientation returns the orientation of the transcript relative to Location.
+func (t *CodingTranscript) Orientation() feat.Orientation {
+	return t.Orient
+}
+
+// UTR5start returns the start of the 5'UTR relative to the transcript.
+func (t *CodingTranscript) UTR5start() int {
+	if t.Orient == feat.Forward {
+		return 0
+	} else if t.Orient == feat.Reverse {
+		return t.CDSend
+	}
+	panic("zero orientation for transcript")
+}
+
+// UTR5end returns the end of the 5'UTR relative to the transcript.
+func (t *CodingTranscript) UTR5end() int {
+	if t.Orient == feat.Forward {
+		return t.CDSstart
+	} else if t.Orient == feat.Reverse {
+		return t.Len()
+	}
+	panic("zero orientation for transcript")
+}
+
+// UTR3start returns the start of the 3'UTR relative to the trnscript.
+func (t *CodingTranscript) UTR3start() int {
+	if t.Orient == feat.Forward {
+		return t.CDSend
+	} else if t.Orient == feat.Reverse {
+		return 0
+	}
+	panic("zero orientation for transcript")
+}
+
+// UTR3end returns the end of the 3'UTR relative to the trnscript.
+func (t *CodingTranscript) UTR3end() int {
+	if t.Orient == feat.Forward {
+		return t.Len()
+	} else if t.Orient == feat.Reverse {
+		return t.CDSstart
+	}
+	panic("zero orientation for transcript")
+}
+
+// Exons returns a typed slice with the transcript exons.
+func (t *CodingTranscript) Exons() Exons { return t.exons }
+
+// Introns returns a typed slice with the transcript introns.
+func (t *CodingTranscript) Introns() Introns { return t.exons.Introns() }
+
+// SetExons sets the transcript exons. Internally, it sorts exons by Start,
+// verifies that their Location is the transcript, that they are not
+// overlapping and that one has zero Start. If an error occurs it is returned
+// and the exons are not set.
+func (t *CodingTranscript) SetExons(exons ...Exon) error {
+	newExons, err := buildExonsFor(t, exons...)
+	if err != nil {
+		return err
+	}
+	t.exons = newExons
+	return nil
+}
+
+// Exons is a typed slice of Exon. It guarantees that exons are always sorted
+// by Start, are all located on the same feature and are non overlapping.
+type Exons []Exon
+
+// SplicedLen returns the total length of the exons.
+func (s Exons) SplicedLen() int {
+	length := 0
+	for _, e := range s {
+		length += e.Len()
+	}
+	return length
+}
+
+// Add adds exons to the slice and safeguards the types contracts. It returns
+// a new slice with the added exons. It checks for sorting, overlap, and
+// location match.  If and error occurs it returns the old slice (without the
+// new exons) and the error.
+func (s Exons) Add(exons ...Exon) (Exons, error) {
+	newSlice := append(s, exons...)
+	sort.Sort(newSlice)
+	for i, e := range newSlice {
+		if i != 0 && e.Start() < newSlice[i-1].End() {
+			return s, errors.New("exons overlap")
+		}
+		if i != 0 && e.Location() != newSlice[i-1].Location() {
+			return s, errors.New("exons location differ")
+		}
+
+	}
+	if s.Location() != nil && s.Location() != newSlice.Location() {
+		return s, errors.New("new exons locations differ from old ones")
+	}
+	return newSlice, nil
+}
+
+// Location returns the common location of all the exons.
+func (s Exons) Location() feat.Feature {
+	if len(s) == 0 {
+		return nil
+	}
+	return s[0].Location()
+}
+
+// Len returns the number of exons in the slice.
+func (s Exons) Len() int {
+	return len(s)
+}
+
+// Less returns whether the exon with index i should sort before
+// the exon with index j.
+func (s Exons) Less(i, j int) bool {
+	return s[i].Start() < s[j].Start()
+}
+
+// Swap swaps the exons with indexes i and j.
+func (s Exons) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// End returns the maximum End of all exons. Since exons are sorted and non
+// overlapping this matches the End of the last exon in the slice.
+func (s Exons) End() int {
+	if len(s) == 0 {
+		return 0
+	}
+	return s[len(s)-1].End()
+}
+
+// Start returns the minimum Start of all exons. Since exons are sorted and
+// non overlapping this matches the Start of the first exon in the slice.
+func (s Exons) Start() int {
+	if len(s) == 0 {
+		return 0
+	}
+	return s[0].Start()
+}
+
+// Introns returns a typed slice of Introns. Iintrons are built dynamically.
+func (s Exons) Introns() Introns {
+	var introns Introns
+	if s.Len() < 2 {
+		return introns
+	}
+	for i := 1; i < s.Len(); i++ {
+		intron := Intron{
+			Transcript: s[i].Transcript,
+			Offset:     s[i-1].End(),
+			Length:     s[i].Start() - s[i-1].End(),
+		}
+		introns = append(introns, intron)
+	}
+	return introns
+}
+
+// An Exon is the part of a transcript that remains present in the final
+// mature RNA product after splicing.
+type Exon struct {
+	Transcript Transcript
+	Offset     int
+	Length     int
+	Desc       string
+}
+
+// Start returns the start position of the exon relative to Transcript.
+func (e Exon) Start() int { return e.Offset }
+
+// End returns the end position of the exon relative to Transcript.
+func (e Exon) End() int { return e.Offset + e.Length }
+
+// Len returns the length of the exon.
+func (e Exon) Len() int { return e.Length }
+
+// Location returns the location of the exon - the transcript.
+func (e Exon) Location() feat.Feature { return e.Transcript }
+
+// Name returns an empty string.
+func (e Exon) Name() string { return "" }
+
+// Description returns a description for the exon.
+func (e Exon) Description() string { return e.Desc }
+
+// Orientation always returns Forward.
+func (e Exon) Orientation() feat.Orientation {
+	return feat.Forward
+}
+
+// Introns corresponds to a collection of introns.
+type Introns []Intron
+
+// An Intron is the part of a transcript that is removed during splicing
+// and is not part of the final mature RNA product.
+type Intron struct {
+	Transcript Transcript
+	Offset     int
+	Length     int
+	Desc       string
+}
+
+// Start returns the start position of the intron relative to Transcript.
+func (i Intron) Start() int { return i.Offset }
+
+// End returns the end position of the intron relative to Transcript.
+func (i Intron) End() int { return i.Offset + i.Length }
+
+// Len returns the length of the intron.
+func (i Intron) Len() int { return i.Length }
+
+// Location returns the location of the intron - the transcript.
+func (i Intron) Location() feat.Feature { return i.Transcript }
+
+// Name returns an empty string.
+func (i Intron) Name() string { return "" }
+
+// Description returns a description for the intron.
+func (i Intron) Description() string { return i.Desc }
+
+// Orientation always returns Forward.
+func (i Intron) Orientation() feat.Orientation {
+	return feat.Forward
+}
+
+// buildExonsFor is a helper function that will check if exons are compatible
+// with a transcript and return a typed slice of exons. If it encounters an
+// error or the exons are not compatible with the transcript it will return
+// the error and a possibly partially filled slice. It is not safe to use the
+// slice if the error is not nil.
+func buildExonsFor(t Transcript, exons ...Exon) (Exons, error) {
+	var newExons Exons
+	newExons, err := newExons.Add(exons...)
+	if err != nil {
+		return newExons, err
+	}
+	if newExons.Location() != t {
+		return newExons, errors.New("exon location is not the transcript")
+	}
+	if newExons.Start() != 0 {
+		return newExons, errors.New("no exon with a zero start")
+	}
+	return newExons, nil
+}
---
+++ feat/gene/gene_test.go
@@ -0,0 +1,398 @@
+// Copyright ©2015 The bíogo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gene
+
+import (
+	"code.google.com/p/biogo/feat"
+
+	"gopkg.in/check.v1"
+	"testing"
+)
+
+// Assert that interfaces are satisfied
+var (
+	_ feat.Feature = (*Gene)(nil)
+	_ feat.Feature = (*NonCodingTranscript)(nil)
+	_ feat.Feature = (*CodingTranscript)(nil)
+	_ feat.Feature = (*Exon)(nil)
+	_ feat.Feature = (*Intron)(nil)
+
+	_ featureOrienter = (*Gene)(nil)
+	_ featureOrienter = (*NonCodingTranscript)(nil)
+	_ featureOrienter = (*CodingTranscript)(nil)
+	_ featureOrienter = (*Exon)(nil)
+	_ featureOrienter = (*Intron)(nil)
+
+	_ Transcript = (*NonCodingTranscript)(nil)
+	_ Transcript = (*CodingTranscript)(nil)
+
+	_ Interface = (*Gene)(nil)
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { check.TestingT(t) }
+
+// Create the test suite
+type S struct{}
+
+var _ = check.Suite(&S{})
+
+// Chr implements feat.Feature and is used as location for the test objects.
+type Chr string
+
+func (c Chr) Start() int             { return 0 }
+func (c Chr) End() int               { return 0 }
+func (c Chr) Len() int               { return 0 }
+func (c Chr) Name() string           { return string(c) }
+func (c Chr) Description() string    { return "chrom" }
+func (c Chr) Location() feat.Feature { return nil }
+
+// Define some test objects that will be used in the actual tests
+var (
+	geneA = Gene{
+		ID:     "A",
+		Chrom:  Chr("Y"),
+		Offset: 100,
+		Orient: feat.Forward,
+		Desc:   "forward gene",
+	}
+	geneB = Gene{
+		ID:     "B",
+		Chrom:  Chr("X"),
+		Offset: 100,
+		Orient: feat.Reverse,
+		Desc:   "reverse gene",
+	}
+	codingTranscriptA = CodingTranscript{
+		ID:       "A1",
+		Loc:      Chr("Y"),
+		Offset:   100,
+		CDSstart: 100,
+		CDSend:   600,
+		Orient:   feat.Forward,
+		Desc:     "forward transcript with cds",
+	}
+	codingTranscriptB = CodingTranscript{
+		ID:       "B1",
+		Loc:      Chr("X"),
+		Offset:   500,
+		CDSstart: 300,
+		CDSend:   1300,
+		Orient:   feat.Reverse,
+		Desc:     "reverse transcript with cds",
+	}
+	nonCodingTranscriptA = NonCodingTranscript{
+		ID:     "C1",
+		Loc:    Chr("Y"),
+		Offset: 100,
+		Orient: feat.Forward,
+		Desc:   "forward non coding transcript",
+	}
+	nonCodingTranscriptB = NonCodingTranscript{
+		ID:     "D1",
+		Loc:    Chr("X"),
+		Offset: 500,
+		Orient: feat.Reverse,
+		Desc:   "reverse non coding transcript",
+	}
+)
+
+// Tests for Gene
+var geneTests = []struct {
+	Test        string
+	Gene        Interface
+	Name        string
+	Chrom       string
+	Start, End  int
+	Len         int
+	Orientation feat.Orientation
+	Feats       []feat.Feature
+	SetErr      string
+	TransCount  int
+}{
+	{
+		Test:        "forward gene with legit feats",
+		Gene:        &geneA,
+		Name:        "A",
+		Chrom:       "Y",
+		Start:       100,
+		End:         120,
+		Len:         20,
+		Orientation: feat.Forward,
+		Feats: []feat.Feature{
+			&NonCodingTranscript{Loc: &geneA, exons: []Exon{{Length: 20}}},
+			&NonCodingTranscript{Loc: &geneA},
+		},
+		TransCount: 2,
+	},
+	{
+		Test:        "reverse gene with legit feats",
+		Gene:        &geneB,
+		Name:        "B",
+		Chrom:       "X",
+		Start:       100,
+		End:         110,
+		Len:         10,
+		Orientation: feat.Reverse,
+		Feats: []feat.Feature{
+			&NonCodingTranscript{Loc: &geneB, exons: []Exon{{Length: 10}}},
+			&NonCodingTranscript{Loc: &geneB},
+		},
+		TransCount: 2,
+	},
+	{
+		Test:   "forward gene with feat on wrong location",
+		Gene:   &geneA,
+		Feats:  []feat.Feature{&NonCodingTranscript{Loc: &geneB}},
+		SetErr: "transcript location does not match the gene",
+	},
+	{
+		Test:   "reverse gene with no feat from 0",
+		Gene:   &geneB,
+		Feats:  []feat.Feature{&NonCodingTranscript{Loc: &geneB, Offset: 5}},
+		SetErr: "no transcript with 0 start on gene",
+	},
+}
+
+func (s *S) TestGene(c *check.C) {
+	for _, d := range geneTests {
+		g := d.Gene
+
+		// Test SetFeatures
+		if err := g.SetFeatures(d.Feats...); err != nil {
+			c.Assert(err, check.ErrorMatches, d.SetErr)
+		} else {
+			c.Check(g.Name(), check.Equals, d.Name)
+			c.Check(g.Start(), check.Equals, d.Start)
+			c.Check(g.End(), check.Equals, d.End)
+			c.Check(g.Len(), check.Equals, d.Len)
+			c.Check(g.Location().Name(), check.Equals, d.Chrom)
+			c.Check(g.Orientation(), check.Equals, d.Orientation)
+			c.Check(len(TranscriptsOf(g)), check.Equals, d.TransCount)
+		}
+	}
+}
+
+// Tests for Transcript
+var transcriptTests = []struct {
+	Test               string
+	Transcript         Transcript
+	Name               string
+	Loc                feat.Feature
+	Start, End         int
+	UTR5start, UTR5end int
+	CDSstart, CDSend   int
+	UTR3start, UTR3end int
+	Len                int
+	Orientation        feat.Orientation
+	Exons              []Exon
+	AddErr             string
+	ExonicLen          int
+}{
+	{
+		Test:        "forward transcript with cds and legit exons",
+		Transcript:  &codingTranscriptA,
+		Name:        "A1",
+		Loc:         Chr("Y"),
+		Orientation: feat.Forward,
+		Exons: []Exon{
+			{Transcript: &codingTranscriptA, Offset: 0, Length: 300},
+			{Transcript: &codingTranscriptA, Offset: 600, Length: 200}},
+		Start:     100,
+		End:       900,
+		UTR5start: 0,
+		UTR5end:   100,
+		CDSstart:  100,
+		CDSend:    600,
+		UTR3start: 600,
+		UTR3end:   800,
+		Len:       800,
+		ExonicLen: 500,
+	},
+	{
+		Test:        "reverse transcript with cds and legit exons",
+		Transcript:  &codingTranscriptB,
+		Name:        "B1",
+		Loc:         Chr("X"),
+		Orientation: feat.Reverse,
+		Exons: []Exon{
+			{Transcript: &codingTranscriptB, Offset: 0, Length: 600},
+			{Transcript: &codingTranscriptB, Offset: 900, Length: 600}},
+		Start:     500,
+		End:       2000,
+		UTR3start: 0,
+		UTR3end:   300,
+		CDSstart:  300,
+		CDSend:    1300,
+		UTR5start: 1300,
+		UTR5end:   1500,
+		Len:       1500,
+		ExonicLen: 1200,
+	},
+	{
+		Test:        "forward non cds transcript with legit exons",
+		Transcript:  &nonCodingTranscriptA,
+		Name:        "C1",
+		Loc:         Chr("Y"),
+		Orientation: feat.Forward,
+		Exons: []Exon{
+			{Transcript: &nonCodingTranscriptA, Offset: 0, Length: 300},
+			{Transcript: &nonCodingTranscriptA, Offset: 600, Length: 200}},
+		Start:     100,
+		End:       900,
+		Len:       800,
+		ExonicLen: 500,
+	},
+	{
+		Test:        "reverse non cds transcript without exon at 0",
+		Transcript:  &nonCodingTranscriptB,
+		Orientation: feat.Reverse,
+		Exons: []Exon{
+			{Transcript: &nonCodingTranscriptB, Offset: 10}},
+		AddErr: "no exon with a zero start",
+	},
+	{
+		Test:        "reverse non cds transcript with wrong exon location",
+		Transcript:  &nonCodingTranscriptB,
+		Orientation: feat.Reverse,
+		Exons:       []Exon{{Offset: 0, Length: 10000}},
+		AddErr:      "exon location is not the transcript",
+	},
+}
+
+func (s *S) TestTranscript(c *check.C) {
+	for _, d := range transcriptTests {
+		t := d.Transcript
+
+		// Test SetExons
+		if err := t.SetExons(d.Exons...); err != nil {
+			c.Assert(err, check.ErrorMatches, d.AddErr)
+		} else {
+			t.Exons()[0].Offset = 1000000 // should have no effect on t
+
+			c.Check(t.Name(), check.Equals, d.Name)
+			c.Check(t.Start(), check.Equals, d.Start)
+			c.Check(t.End(), check.Equals, d.End)
+			c.Check(t.Len(), check.Equals, d.Len)
+			c.Check(t.Location(), check.Equals, d.Loc)
+			c.Check(t.Orientation(), check.Equals, d.Orientation)
+			c.Check(t.Exons().SplicedLen(), check.Equals, d.ExonicLen)
+
+			// Test CodingTranscript specifics
+			if t, ok := t.(*CodingTranscript); ok {
+				c.Check(t.CDSstart, check.Equals, d.CDSstart)
+				c.Check(t.CDSend, check.Equals, d.CDSend)
+				c.Check(t.UTR5start(), check.Equals, d.UTR5start)
+				c.Check(t.UTR5end(), check.Equals, d.UTR5end)
+				c.Check(t.UTR3start(), check.Equals, d.UTR3start)
+				c.Check(t.UTR3end(), check.Equals, d.UTR3end)
+			}
+		}
+	}
+}
+
+// Tests for Exon and Intron
+type featureOrienter interface {
+	feat.Orienter
+	feat.Feature
+}
+
+var exonIntronTests = []struct {
+	Test        string
+	Feat        featureOrienter
+	Start, End  int
+	Len         int
+	Transcript  feat.Feature
+	Orientation feat.Orientation
+}{
+	{
+		Test:        "Exon on transcript",
+		Feat:        Exon{Offset: 200, Length: 200},
+		Start:       200,
+		End:         400,
+		Len:         200,
+		Orientation: feat.Forward,
+	},
+	{
+		Test:        "Intron on transcript",
+		Feat:        Intron{Offset: 300, Length: 500},
+		Start:       300,
+		End:         800,
+		Len:         500,
+		Orientation: feat.Forward,
+	},
+}
+
+func (s *S) TestExonIntron(c *check.C) {
+	for _, d := range exonIntronTests {
+		e := d.Feat
+
+		c.Check(e.Start(), check.Equals, d.Start)
+		c.Check(e.End(), check.Equals, d.End)
+		c.Check(e.Len(), check.Equals, d.Len)
+		c.Check(e.Location(), check.DeepEquals, d.Transcript)
+		c.Check(e.Orientation(), check.Equals, d.Orientation)
+	}
+}
+
+// Tests for Exons
+var exonsTests = []struct {
+	Test                        string
+	InputExons                  []Exon
+	Location                    feat.Feature
+	Start, End, Len, SplicedLen int
+	AddErr                      string
+	MadeIntrons                 Introns
+}{
+	{
+		Test: "Exons not in order",
+		InputExons: []Exon{
+			Exon{Offset: 300, Length: 100},
+			Exon{Offset: 0, Length: 100},
+		},
+		Start:      0,
+		End:        400,
+		Len:        2,
+		SplicedLen: 200,
+		MadeIntrons: Introns{
+			Intron{Offset: 100, Length: 200},
+		},
+	},
+	{
+		Test: "Exons overlap",
+		InputExons: []Exon{
+			Exon{Offset: 0, Length: 100},
+			Exon{Offset: 50, Length: 100},
+		},
+		AddErr: "exons overlap",
+	},
+	{
+		Test: "Exons on different transcripts",
+		InputExons: []Exon{
+			Exon{Transcript: &codingTranscriptA},
+			Exon{Transcript: &codingTranscriptB},
+		},
+		AddErr: "exons location differ",
+	},
+}
+
+func (s *S) TestExons(c *check.C) {
+	for _, d := range exonsTests {
+		var e Exons
+		ie := d.InputExons
+
+		// Test SetExons
+		if e, err := e.Add(ie...); err != nil {
+			c.Assert(err, check.ErrorMatches, d.AddErr)
+		} else {
+			c.Check(e.Location(), check.DeepEquals, d.Location)
+			c.Check(e.Start(), check.Equals, d.Start)
+			c.Check(e.End(), check.Equals, d.End)
+			c.Check(e.Len(), check.Equals, d.Len)
+			c.Check(e.SplicedLen(), check.Equals, d.SplicedLen)
+			c.Check(e.Introns(), check.DeepEquals, d.MadeIntrons)
+		}
+	}
+}
